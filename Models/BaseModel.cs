be9a32ce344be2299dcb2cec7373570485e0776c
Ryan Peters <ryan@binarydad.com>
2020-09-15 18:13:19 -0400

    moved BaseModel to own file

---
+++ Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinaryDad.AggregateDal.Models
+{
+    public class BaseModel
+    {
+        /// <summary>
+        /// The SQL key ID
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Quickbase Record ID
+        /// </summary>
+        public int RecordId { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; }
+        public DateTime Modified { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
